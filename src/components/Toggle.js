cc5e130e74e9440baf62b150d79ad1b05ce78c03
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-08-03 15:19:32 +0300

    toggle

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -6,7 +6,13 @@ function Toggle() {
   function handleClick(){
     setIsOn((isOn) => !isOn);
   }
-  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button 
+      style={{background:color}} onClick={handleClick}>{isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
